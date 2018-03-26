c439536d3918e122d579543305021a6eff6cbd87
John Kalinauskas <jkalinauskas@live.com>
2018-03-26 22:34:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts() {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var array = [];
   var i = 0;
-  while (array.length < facts.length) {
+  while (array.length < facts.length;i++) {
     array.push(facts[i] + "!!!");
     i++;
   }
