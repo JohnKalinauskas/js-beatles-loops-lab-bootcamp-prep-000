4fad6b70ed71f09d6362daee78cab64035f001ea
John Kalinauskas <jkalinauskas@live.com>
2018-03-26 21:24:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+function iLoveTheBeatles (number){
+    var newArray = [];
+    do {
+      newArray.push("I love the Beatles!");
+     number = number++;
+    } while (number < 15); 
+    return newArray;
+}
