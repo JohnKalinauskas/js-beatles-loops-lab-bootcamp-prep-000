f0d5af0f682ad8014c147f73be05bf57e8d9dd34
John Kalinauskas <jkalinauskas@live.com>
2018-03-26 21:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for (let i = 2; i < 4; i++) {
+  for (let i = 3; i < 4; i++) {
       array.push(`${musicians[0]} plays ${instruments[0]}`);
     }
   return array;
