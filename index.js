a87138d9d9dd38f5a895b11c6b9427acba128bb4
John Kalinauskas <jkalinauskas@live.com>
2018-03-26 21:28:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,3 @@
 
 
 
-function iLoveTheBeatles (number){
-    var newArray = [];
-    do {
-      newArray.push("I love the Beatles!");
-     number = number++;
-    } while (number < 15); 
-    return newArray;
-}
