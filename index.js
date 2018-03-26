5dbea12692d4d3b8aa98924217a0a31f939375ce
John Kalinauskas <jkalinauskas@live.com>
2018-03-26 21:37:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,34 +11,6 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-theBeatlesPlay(musicians, instruments);
-
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-function johnLennonFacts(facts) {
-  while () {
-    facts[n] + "!!!";
-  }
-  return facts;  
-}
-
-function iLoveTheBeatles(x) {
-  var array = [];
-  do {
-    x = array.push("I love the Beatles!");
-    x++;
-  } while (x < 15); {
-    
-  }
-  return array;
-}
-
-iLoveTheBeatles(12);
 
 
 
