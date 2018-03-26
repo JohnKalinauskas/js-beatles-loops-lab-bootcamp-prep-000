78940b814a273c0fba1d053a78c30c35e64b79e6
John Kalinauskas <jkalinauskas@live.com>
2018-03-26 22:27:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 
-function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (let i = 1; i < 1; i++) {
-      array.push(`${musicians[1]} plays ${instruments[1]}`);
-    }
-  return array;
-}
+
 
 
 
